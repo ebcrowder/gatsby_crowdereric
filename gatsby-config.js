7304188be85fe22250b12d9f87baab2b71e69b2c
ebcrowder <ebcrowder@gmail.com>
2018-06-07 15:11:28 -0700

    google tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,14 +12,6 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: 'UA-115921525-1',
-        // Puts tracking script in the head instead of the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**', '/do-not-track/me/too/'],
       },
       resolve: `gatsby-plugin-favicon`,
       options: {
