d262f90bec2a6d5d0918ca866437575fddd721a9
ebcrowder <ebcrowder@gmail.com>
2018-06-07 15:05:53 -0700

    google tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
       options: {
         trackingId: 'UA-115921525-1',
         // Puts tracking script in the head instead of the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
