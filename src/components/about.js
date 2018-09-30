bede4cc2709a0440d74beae8d4fa31a2109f2391
Eric Crowder <ericcrowder@Erics-MacBook-Pro.local>
2018-09-29 17:27:46 -0700

    fix card elements

--- src/components/about.js
+++ src/components/about.js
@@ -1,100 +1,75 @@
-import React, { Component } from 'react';
-import posed from 'react-pose';
+import React from 'react';
 import Layout from './layout';
 
 import Me from '../img/about.jpg';
 import Jefferson from '../img/jefferson.jpg';
 
-const CardAnimate = posed.div({
-  idle: { scale: 1 },
-  hovered: { scale: 1.01 },
-});
-
-export default class About extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      hovering1: false,
-      hovering2: false,
-    };
-  }
-
-  render() {
-    const { hovering1, hovering2 } = this.state;
-    return (
-      <Layout>
-        <div>
-          <h1>about</h1>
-          <div className="about">
-            <CardAnimate
-              className="card"
-              pose={hovering1 ? 'hovered' : 'idle'}
-              onMouseEnter={() => this.setState({ hovering1: true })}
-              onMouseLeave={() => this.setState({ hovering1: false })}
-            >
-              <img className="card-img-top-about" src={Me} alt="about" />
-              <div className="card-body">
-                <p className="card-text">me!</p>
-              </div>
-            </CardAnimate>
-            <CardAnimate
-              className="card"
-              pose={hovering2 ? 'hovered' : 'idle'}
-              onMouseEnter={() => this.setState({ hovering2: true })}
-              onMouseLeave={() => this.setState({ hovering2: false })}
-            >
-              <img className="card-img-top-about" src={Jefferson} alt="jefferson" />
-              <div className="card-body">
-                <p className="card-text">jefferson!</p>
-              </div>
-            </CardAnimate>
-          </div>
-          <div className="bio">
-            <p className="lead">
-              I am a full-stack web developer based in sunny Portland, Oregon. In particular, I
-              really enjoy working with various JavaScript libraries and runtimes such as React.js
-              and Node.js. As a former accountant of 10+ years, I thrive when working with engaged
-              teams to find solutions for clients. I believe in immersing myself fully in life and
-              thereby fully enjoying the process, whether it be work or personal endeavors.
-            </p>
-            <p className="lead">
-              When not coding, I am an avid trail runner and spend a lot of time running in gorgeous
-              Forest Park or on the endless trails in Oregon and Washington. I ran my first
-              ultra-distance marathon in 2017 (Oregon Coast 50k) and love being a part of the trail
-              running community here in the Pacific Northwest. I also really enjoy hiking and
-              camping with my dog, Jefferson.
-            </p>
+const About = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>about</h1>
+        <div className="about">
+          <div className="card">
+            <img className="card-img-top-about" src={Me} alt="about" />
+            <div className="card-body">
+              <p className="card-text">me!</p>
+            </div>
           </div>
 
-          <div className="my-auto">
-            <h4 className="text-uppercase">Skills</h4>
-
-            <div className="subheading mb-3">Programming Languages & Tools:</div>
-            <ul className="code-languages">
-              <li>
-                <i className="fab fa-html5 fa-2x" />
-                <i className="fab fa-css3-alt fa-2x" />
-                <i className="fab fa-js fa-2x" />
-                <i className="fab fa-react fa-2x" />
-                <i className="fab fa-node fa-2x" />
-                <i className="fas fa-database fa-2x" />
-              </li>
-            </ul>
+          <div className="card">
+            <img className="card-img-top-about" src={Jefferson} alt="jefferson" />
+            <div className="card-body">
+              <p className="card-text">jefferson!</p>
+            </div>
           </div>
+        </div>
+        <div className="bio">
+          <p className="lead">
+            I am a full-stack web developer based in sunny Portland, Oregon. In particular, I really
+            enjoy working with various JavaScript libraries and runtimes such as React.js and
+            Node.js. As a former accountant of 10+ years, I thrive when working with engaged teams
+            to find solutions for clients. I believe in immersing myself fully in life and thereby
+            fully enjoying the process, whether it be work or personal endeavors.
+          </p>
+          <p className="lead">
+            When not coding, I am an avid trail runner and spend a lot of time running in gorgeous
+            Forest Park or on the endless trails in Oregon and Washington. I ran my first
+            ultra-distance marathon in 2017 (Oregon Coast 50k) and love being a part of the trail
+            running community here in the Pacific Northwest. I also really enjoy hiking and camping
+            with my dog, Jefferson.
+          </p>
+        </div>
 
-          <h4 className="text-uppercase">Education</h4>
-          <ul>
-            <li>The Tech Academy - Software Developer Certificate - December 2017</li>
-            <li>Longwood University - BSBA - Accounting - May 2007 </li>
-          </ul>
+        <div className="my-auto">
+          <h4 className="text-uppercase">Skills</h4>
 
-          <h4 className="text-uppercase">Certifications</h4>
-          <ul>
-            <li>Amazon Web Services (AWS) Certified Developer - Associate</li>
+          <div className="subheading mb-3">Programming Languages & Tools:</div>
+          <ul className="code-languages">
+            <li>
+              <i className="fab fa-html5 fa-2x" />
+              <i className="fab fa-css3-alt fa-2x" />
+              <i className="fab fa-js fa-2x" />
+              <i className="fab fa-react fa-2x" />
+              <i className="fab fa-node fa-2x" />
+              <i className="fas fa-database fa-2x" />
+            </li>
           </ul>
         </div>
-      </Layout>
-    );
-  }
-}
+
+        <h4 className="text-uppercase">Education</h4>
+        <ul>
+          <li>The Tech Academy - Software Developer Certificate - December 2017</li>
+          <li>Longwood University - BSBA - Accounting - May 2007 </li>
+        </ul>
+
+        <h4 className="text-uppercase">Certifications</h4>
+        <ul>
+          <li>Amazon Web Services (AWS) Certified Developer - Associate</li>
+        </ul>
+      </div>
+    </Layout>
+  );
+};
+
+export default About;
--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import posed from 'react-pose';
+import React from 'react';
 import Layout from './layout';
 
 import darling from '../img/portfolio/darling.jpg';
@@ -9,151 +8,99 @@ import ether from '../img/portfolio/ether.jpg';
 import reverb from '../img/portfolio/reverb.jpg';
 import trailer from '../img/portfolio/trailer.jpg';
 
-const CardAnimate = posed.div({
-  idle: { scale: 1 },
-  hovered: { scale: 1.01 },
-});
+const Portfolio = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>portfolio</h1>
+        <div className="card-deck">
+          <div className="card">
+            <img className="card-img-top" src={trailer} alt="trailer" />
+            <div className="card-body">
+              <h5 className="card-title">
+                <a href="https://obscure-everglades-66951.herokuapp.com/">trailer mountain</a>
+              </h5>
+              <p className="card-text">
+                utilizing the MovieDB (TMDb), allows users to search the TMDb for movies and view
+                related info, posters and trailers.
+              </p>
+            </div>
+          </div>
 
-export default class Portfolio extends Component {
-  constructor(props) {
-    super(props);
+          <div className="card">
+            <img className="card-img-top" src={darling} alt="darling" />
+            <div className="card-body">
+              <h5 className="card-title">
+                <a href="http://darlingdance.com/">darlingdance</a>
+              </h5>
+              <p className="card-text">
+                founded in 2010, darlingdance is a feminist postmodern dance company based in
+                Washington, DC.
+              </p>
+            </div>
+          </div>
 
-    this.state = {
-      hovering1: false,
-      hovering2: false,
-      hovering3: false,
-      hovering4: false,
-      hovering5: false,
-      hovering6: false,
-    };
-  }
+          <div className="card">
+            <img className="card-img-top" src={reverb} alt="reverb" />
+            <div className="card-body">
+              <h5 className="card-title">
+                <a href="https://sheltered-beyond-71050.herokuapp.com/">reverb</a>
+              </h5>
+              <p className="card-text">
+                single page application for searching the Discogs database for albums by artist.
+                Built with React and features CSS Grid and Styled Components.
+              </p>
+            </div>
+          </div>
 
-  render() {
-    const {
-      hovering1, hovering2, hovering3, hovering4, hovering5, hovering6,
-    } = this.state;
-    return (
-      <Layout>
-        <div>
-          <h1>portfolio</h1>
-          <div className="card-deck">
-            <CardAnimate
-              className="card"
-              pose={hovering4 ? 'hovered' : 'idle'}
-              onMouseEnter={() => this.setState({ hovering4: true })}
-              onMouseLeave={() => this.setState({ hovering4: false })}
-            >
-              <img className="card-img-top" src={trailer} alt="trailer" />
-              <div className="card-body">
-                <h5 className="card-title">
-                  <a href="https://obscure-everglades-66951.herokuapp.com/">trailer mountain</a>
-                </h5>
-                <p className="card-text">
-                  utilizing the MovieDB (TMDb), allows users to search the TMDb for movies and view
-                  related info, posters and trailers.
-                </p>
-              </div>
-            </CardAnimate>
+          <div className="card">
+            <img className="card-img-top" src={serverless} alt="serverless" />
+            <div className="card-body">
+              <h5 className="card-title">
+                <a href="http://serverless-e-crowder.s3-website-us-east-1.amazonaws.com/">
+                  serverless blog
+                </a>
+              </h5>
+              <p className="card-text">
+                blog application featuring CRUD functionality. front-end built with React.js and
+                Redux. REST API built with Express and Node.js. deployed on aws using Serverless
+                framework.
+              </p>
+            </div>
+          </div>
 
-            <CardAnimate
-              className="card"
-              pose={hovering2 ? 'hovered' : 'idle'}
-              onMouseEnter={() => this.setState({ hovering2: true })}
-              onMouseLeave={() => this.setState({ hovering2: false })}
-            >
-              <img className="card-img-top" src={darling} alt="darling" />
-              <div className="card-body">
-                <h5 className="card-title">
-                  <a href="http://darlingdance.com/">darlingdance</a>
-                </h5>
-                <p className="card-text">
-                  founded in 2010, darlingdance is a feminist postmodern dance company based in
-                  Washington, DC.
-                </p>
-              </div>
-            </CardAnimate>
+          <div className="card">
+            <img className="card-img-top" src={tree} alt="tree" />
+            <div className="card-body">
+              <h5 className="card-title">
+                <a href="https://sheltered-brushlands-51479.herokuapp.com/">tree</a>
+              </h5>
+              <p className="card-text">
+                open source management software for health & wellness businesses. features
+                authentication via OAuth 2.0, scheduling, and other functionality. built using
+                React.js, Redux, Node.js, Express and MongoDB.
+              </p>
+            </div>
+          </div>
 
-            <CardAnimate
-              className="card"
-              pose={hovering3 ? 'hovered' : 'idle'}
-              onMouseEnter={() => this.setState({ hovering3: true })}
-              onMouseLeave={() => this.setState({ hovering3: false })}
-            >
-              <img className="card-img-top" src={reverb} alt="reverb" />
-              <div className="card-body">
-                <h5 className="card-title">
-                  <a href="https://sheltered-beyond-71050.herokuapp.com/">reverb</a>
-                </h5>
-                <p className="card-text">
-                  single page application for searching the Discogs database for albums by artist.
-                  Built with React and features CSS Grid and Styled Components.
-                </p>
-              </div>
-            </CardAnimate>
-
-            <CardAnimate
-              className="card"
-              pose={hovering1 ? 'hovered' : 'idle'}
-              onMouseEnter={() => this.setState({ hovering1: true })}
-              onMouseLeave={() => this.setState({ hovering1: false })}
-            >
-              <img className="card-img-top" src={serverless} alt="serverless" />
-              <div className="card-body">
-                <h5 className="card-title">
-                  <a href="http://serverless-e-crowder.s3-website-us-east-1.amazonaws.com/">
-                    serverless blog
-                  </a>
-                </h5>
-                <p className="card-text">
-                  blog application featuring CRUD functionality. front-end built with React.js and
-                  Redux. REST API built with Express and Node.js. deployed on aws using Serverless
-                  framework.
-                </p>
-              </div>
-            </CardAnimate>
-
-            <CardAnimate
-              className="card"
-              pose={hovering5 ? 'hovered' : 'idle'}
-              onMouseEnter={() => this.setState({ hovering5: true })}
-              onMouseLeave={() => this.setState({ hovering5: false })}
-            >
-              <img className="card-img-top" src={tree} alt="tree" />
-              <div className="card-body">
-                <h5 className="card-title">
-                  <a href="https://sheltered-brushlands-51479.herokuapp.com/">tree</a>
-                </h5>
-                <p className="card-text">
-                  open source management software for health & wellness businesses. features
-                  authentication via OAuth 2.0, scheduling, and other functionality. built using
-                  React.js, Redux, Node.js, Express and MongoDB.
-                </p>
-              </div>
-            </CardAnimate>
-
-            <CardAnimate
-              className="card"
-              pose={hovering6 ? 'hovered' : 'idle'}
-              onMouseEnter={() => this.setState({ hovering6: true })}
-              onMouseLeave={() => this.setState({ hovering6: false })}
-            >
-              <img className="card-img-top" src={ether} alt="ether" />
-              <div className="card-body">
-                <h5 className="card-title">
-                  <a href="http://cointracker.s3-website-us-east-1.amazonaws.com/">
-                    bitcoin & ethereum price chart
-                  </a>
-                </h5>
-                <p className="card-text">
-                  Ethereum and Bitcoin price history graph built using React.js. It features API
-                  calls via axios to the Coinbase and Poloniex APIs and graphs rendered via
-                  Recharts.
-                </p>
-              </div>
-            </CardAnimate>
+          <div className="card">
+            <img className="card-img-top" src={ether} alt="ether" />
+            <div className="card-body">
+              <h5 className="card-title">
+                <a href="http://cointracker.s3-website-us-east-1.amazonaws.com/">
+                  bitcoin & ethereum price chart
+                </a>
+              </h5>
+              <p className="card-text">
+                Ethereum and Bitcoin price history graph built using React.js. It features API calls
+                via axios to the Coinbase and Poloniex APIs and graphs rendered via Recharts.
+              </p>
+            </div>
           </div>
         </div>
-      </Layout>
-    );
-  }
-}
+      </div>
+    </Layout>
+  );
+};
+
+export default Portfolio;
