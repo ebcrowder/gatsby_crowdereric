dbff6bb09fc4f3f2a49b42570c85760a5956544d
ebcrowder <ebcrowder@gmail.com>
2019-04-05 19:06:20 -0700

    remove page transitions

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,26 +1,7 @@
 import * as React from 'react';
-import posed, { PoseGroup } from 'react-pose';
 import Layout from './src/components/Layout';
-
-const Transition = posed.div({
-  enter: {
-    opacity: 1,
-    delay: 300,
-    applyAtStart: { display: 'grid' },
-    beforeChildren: true,
-  },
-  exit: { opacity: 0 },
-});
 
 export const replaceComponentRenderer = ({ props, ...other }) => {
   const { component } = props.pageResources;
-  return (
-    <Layout>
-      <PoseGroup>
-        <Transition key={props.location.key}>
-          {React.createElement(component, props)}
-        </Transition>
-      </PoseGroup>
-    </Layout>
-  );
+  return <Layout>{React.createElement(component, props)}</Layout>;
 };
