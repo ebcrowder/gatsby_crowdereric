6f78c657c6e7b152c09a6d134d89a313e898795d
ebcrowder <ebcrowder@gmail.com>
2019-04-07 17:41:14 -0700

    fix blog post template

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { graphql } from 'gatsby';
+import Layout from '../components/Layout';
 
 interface PageQueryProps {
   data: {
@@ -18,16 +19,19 @@ const Template: React.FunctionComponent<PageQueryProps> = ({ data }) => {
   const { markdownRemark } = data;
   const { frontmatter, html } = markdownRemark;
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+    <>
+      <Layout />
+      <div className="blog-post-container">
+        <div className="blog-post">
+          <h1>{frontmatter.title}</h1>
+          <h2>{frontmatter.date}</h2>
+          <div
+            className="blog-post-content"
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
