79cc58ac0be464aa6fbd81604e9ea158991ae954
ebcrowder <ebcrowder@gmail.com>
2019-08-02 21:47:33 -0700

    last changes

--- __tests__/contact.test.tsx
+++ __tests__/contact.test.tsx
@@ -1,4 +1,4 @@
-import { render } from 'react-testing-library';
+import { render } from '@testing-library/react';
 import * as React from 'react';
 import Contact from '../src/components/Contact';
 
--- __tests__/splash.test.tsx
+++ __tests__/splash.test.tsx
@@ -1,4 +1,4 @@
-import { render } from 'react-testing-library';
+import { render } from '@testing-library/react';
 import * as React from 'react';
 import Splash from '../src/components/Splash';
 
